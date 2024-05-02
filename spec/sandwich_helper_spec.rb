1478f82827da1559112984a5e234db29793ca335
allancodenet <allancodenet@gmail.com>
2024-05-02 22:07:44 +0300

    using let helper

---
+++ spec/sandwich_helper_spec.rb
@@ -0,0 +1,17 @@
+Sandwich = Struct.new(:taste, :toppings)
+
+RSpec.describe "An Ideal Sandwich" do
+  # def sandwich
+  #   Sandwich.new("delicious", [])
+  # end
+  let(:sandwich) { Sandwich.new("delicious", []) }
+
+  it "is delicious" do
+    taste = sandwich.taste
+    expect(taste).to eq("delicious")
+  end
+  it "lets me add toppings " do
+    toppings = sandwich.toppings << "cheese"
+    expect(toppings).not_to be_empty
+  end
+end
